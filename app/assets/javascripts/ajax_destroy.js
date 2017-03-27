65db75b4f37cbd9246da58f3897d8ab49b299e33
jasonleonhard <devbrights@gmail.com>
2017-03-27 11:16:17 -0700

    use dropdown menues for item actions, and target correctly with ajax

--- app/assets/javascripts/ajax_destroy.js
+++ app/assets/javascripts/ajax_destroy.js
@@ -1,8 +1,12 @@
+// ajax impimentation of destroy
 $(document).on('turbolinks:load', function(){
   $('.destroy').on('click', function(){
     if(confirm ("Are you sure you wish to delete this location?")){
       $.ajax({
-        url: '/locations/' + this.parentElement.id,
+      // if just a button and not a drop down
+        // url: '/locations/' + this.parentElement.id,
+      // if in a bootstrap dropdown menu
+        url: '/locations/' + this.parentElement.parentElement.parentElement.id,
         type: 'DELETE',
         success: function(response){
           // console.log(response);
